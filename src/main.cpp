64a78934f3cd7064d859911074681ac3833a7707
whoany <120975118+whoany@users.noreply.github.com>
2026-05-30 15:36:22 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@ void printFreq(BST<std::string>& tree);
 int main() {
   const char* filename = "src/war_peace.txt";
   BST<std::string> tree;
- 
   makeTree(tree, filename);
   std::cout << "Tree depth : " << tree.depth() << std::endl;
   std::cout << "pierre     : " << tree.search("pierre") << std::endl;
