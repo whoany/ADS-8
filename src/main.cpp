3cd56972935793d9e94fd758339127cedb2d48c9
whoany <120975118+whoany@users.noreply.github.com>
2026-05-30 15:17:09 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <string>
 #include "bst.h"
 
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
 int main() {
-    return 0;
+	const char* filename = "../src/war_peace.txt";
+	BST<std::string> tree;
+
+	makeTree(tree, filename);
+
+	std::cout << "Tree depth : " << tree.depth() << std::endl;
+	std::cout << "pierre     : " << tree.search("pierre") << std::endl;
+
+	printFreq(tree);
+	return 0;
 }
